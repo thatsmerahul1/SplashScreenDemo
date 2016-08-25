249bce01bfec007e43fd17ce7f24e05e1b456d21
Rahul Maurya <thatsmerahul1@gmail.com>
2016-08-25 15:31:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/a20113966/splashscreendemo/SetupPhone.java
@@ -0,0 +1,16 @@
+package com.example.a20113966.splashscreendemo;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 20113966 on 23-08-2016.
+ */
+public class SetupPhone extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.setup_phone_no);
+    }
+}
